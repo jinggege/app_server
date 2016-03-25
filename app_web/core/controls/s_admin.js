9242fa5d8a964ca35416658fa882d12cf5a90230
majing <majing@youku.com>
2016-03-25 18:37:00 +0800

    add admin main menu
    add admin main menu

--- app_web/core/controls/s_admin.js
+++ app_web/core/controls/s_admin.js
@@ -11,25 +11,19 @@ var GL_Admin = function(){};
 GL_Admin.prototype = {
     getControl:function*(){
         var query = url.parse(this.request.url,true).query;
-
         var dataM = single.getSingleClass(single.getCKEY().DATA_MANAGE);
 
         var actionType = query.ac;
         var gId = query.gid;
 
-        console.log('------',actionType);
-
         var resp = {};
         switch(actionType){
             case 'gdel':
-
                 //todo 需要改为异步操作
                var operateFlag =  dataM.delGoodsById(gId);
-
                 resp.response = operateFlag? {data:gId,code:0,msg:'success'} : {data:gId,code:1,msg:'fail'};
                 this.body = JSON.stringify(resp);
                 break;
-
             case 'gin':
                     var gInfo = {};
                     gInfo.url = query.url;
@@ -43,14 +37,8 @@ GL_Admin.prototype = {
 
                 break;
 
-            case 'page':
-                this.body = yield render('gl_admin_nav', {});
-                break;
-
             default :
-
                 var renderData = {};
-
                 renderData.allGoodsList = dataM.getAllGoods();
                 renderData.STATIC_DOMAIN = global.appConfig.app_static_domain;
                 this.body = yield render('gl_admin', renderData);
