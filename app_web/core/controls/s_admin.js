8040e4ef0eda6f9f6c48f331277117b74018f4df
majing <majing@youku.com>
2016-03-28 13:57:44 +0800

    change admin-config
    change admin-config

--- app_web/core/controls/s_admin.js
+++ app_web/core/controls/s_admin.js
@@ -2,7 +2,7 @@
  * Created by dell on 2016/3/24.
  */
 
-var url         = require("url");
+var url          = require("url");
 var render      = require(global.rootUrl+'/lib/render.js');
 var single      = require(global.rootUrl+'/lib/singleclass.js');
 
@@ -15,6 +15,7 @@ GL_Admin.prototype = {
 
         var actionType = query.ac;
         var gId = query.gid;
+        var renderData = {};
 
         var resp = {};
         switch(actionType){
@@ -22,7 +23,7 @@ GL_Admin.prototype = {
                 //todo 需要改为异步操作
                var operateFlag =  dataM.delGoodsById(gId);
                 resp.response = operateFlag? {data:gId,code:0,msg:'success'} : {data:gId,code:1,msg:'fail'};
-                this.body = JSON.stringify(resp);
+                this.body     = JSON.stringify(resp);
                 break;
             case 'gin':
                     var gInfo   = {};
@@ -32,14 +33,17 @@ GL_Admin.prototype = {
 
                     //todo 需要改为异步操作
                     var increaseFlag = dataM.increaseGood(gInfo);
-                    resp.response = increaseFlag? {data:'inscrease success',code:0,msg:'success'} : {data:'',code:1,msg:'fail'};
-                    this.body = JSON.stringify(resp);
-
+                    resp.response   = increaseFlag? {data:'inscrease success',code:0,msg:'success'} : {data:'',code:1,msg:'fail'};
+                    this.body       = JSON.stringify(resp);
                 break;
 
+            case'config':
+                renderData.allGoodsList  = dataM.getAllGoods();
+                renderData.STATIC_DOMAIN = global.appConfig.app_static_domain;
+                this.body = yield render('gl_admin_config', renderData);
+                break;
             default :
-                var renderData = {};
-                renderData.allGoodsList = dataM.getAllGoods();
+                renderData.allGoodsList  = dataM.getAllGoods();
                 renderData.STATIC_DOMAIN = global.appConfig.app_static_domain;
                 this.body = yield render('gl_admin', renderData);
                 break;
